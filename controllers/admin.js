67061d6a4cbac7f87d058fefdc562f3bcfa34087
ashishsharma157 <sharma.ashish78@gmail.com>
2024-12-26 18:49:50 -0800

    Async request

--- controllers/admin.js
+++ controllers/admin.js
@@ -192,16 +192,27 @@ exports.getProducts = (req, res, next) => {
     });
 };
 
-exports.postDeleteProduct = (req, res, next) => {
-  const prodId = req.body.productId;
+exports.deleteProduct = (req, res, next) => {
+
+
+    const prodId = req.params.productId;
   Product.deleteOne({ _id: prodId, userId: req.user._id })
     .then(() => {
       console.log("DESTROYED PRODUCT");
-      res.redirect("/admin/products");
+      res.status(200).json({message:'Success'});
     })
     .catch((err) => {
-      const error = new Error(err);
-      error.httpStatusCode = 500;
-      return next(error);
+      res.status(500).json({message:'Failed'});
     });
+  // const prodId = req.body.productId;
+  // Product.deleteOne({ _id: prodId, userId: req.user._id })
+  //   .then(() => {
+  //     console.log("DESTROYED PRODUCT");
+  //     res.redirect("/admin/products");
+  //   })
+  //   .catch((err) => {
+  //     const error = new Error(err);
+  //     error.httpStatusCode = 500;
+  //     return next(error);
+  //   });
 };
